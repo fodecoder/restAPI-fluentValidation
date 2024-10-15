9b58a74fb26b5a664a0f6c8a0ceffc6d40e05a58
Andrea Foderaro <fodecoder@gmail.com>
2024-10-15 17:04:22 +0200

    Add services

---
+++ RestAPI.FluentValidation.Services/AutoMapper/InventoryProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RestAPI.FluentValidation.Interfaces.Model;
+using DB = Common.DB.Model;
+
+namespace RestAPI.FluentValidation.Services.AutoMapper
+{
+    public class InventoryProfile : Profile
+    {
+        public InventoryProfile()
+        {
+            CreateMap<DB.Item , Item> ();
+        }
+    }
+}
