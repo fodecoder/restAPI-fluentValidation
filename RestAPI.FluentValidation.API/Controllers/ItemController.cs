8b4a8c06572005f6d74842c8aeba8dd0887dafd1
Andrea Foderaro <fodecoder@gmail.com>
2024-10-15 17:11:06 +0200

    Add API's controllers

---
+++ RestAPI.FluentValidation.API/Controllers/ItemController.cs
@@ -0,0 +1,61 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.FluentValidation.Interfaces.Model;
+using RestAPI.FluentValidation.Interfaces.Service;
+
+namespace RestAPI.FluentValidation.API.Controllers
+{
+    [ApiController]
+    [ApiVersion ( 1.0 , Deprecated = true )]
+    [ApiVersion ( 2.0 )]
+    [Route ( "v{version:apiVersion}/[controller]" )]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        private readonly ILogger<ItemController> _logger;
+
+        public ItemController( ILogger<ItemController> logger , IItemService itemService )
+        {
+            _itemService = itemService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task CreateItem( [FromBody] Item item )
+        {
+            _logger.Log ( LogLevel.Debug , $"Created Item: {item}" );
+            await _itemService.CreateItemAsync ( item );
+        }
+
+        [HttpGet ( "{id}" )]
+        public Task<Item> ReadItem( [FromRoute] Guid id )
+        {
+            _logger.Log ( LogLevel.Debug , $"Requested Item: {id}" );
+            return _itemService.ReadItemAsync ( id );
+        }
+
+        [HttpGet ( "{id}" ), MapToApiVersion ( "2.0" )]
+        public async Task<Item> ReadItem2( [FromRoute] Guid id )
+        {
+            _logger.Log ( LogLevel.Debug , $"Requested Item: {id}" );
+            var retValue = await _itemService.ReadItemAsync ( id );
+            retValue.Description += " API version 2.0";
+            return retValue;
+        }
+
+        [HttpPut]
+        public async Task UpdateItem( [FromBody] Item item )
+        {
+            _logger.Log ( LogLevel.Debug , $"Updated Item: {item}" );
+            await _itemService.UpdateItemAsync ( item );
+        }
+
+        [HttpDelete ( "{id}" )]
+        public async Task DeleteItem( Guid id )
+        {
+            _logger.Log ( LogLevel.Debug , $"Deleted Item: {id}" );
+            await _itemService.DeleteItemAsync ( id );
+        }
+    }
+}
---
+++ RestAPI.FluentValidation.API/Program.cs
@@ -0,0 +1,85 @@
+using Asp.Versioning;
+using Common.DB.Extension;
+using Microsoft.Extensions.Options;
+using RestAPI.FluentValidation.API.Config;
+using RestAPI.FluentValidation.Interfaces.Repository;
+using RestAPI.FluentValidation.Interfaces.Service;
+using RestAPI.FluentValidation.Services.AutoMapper;
+using RestAPI.FluentValidation.Services.Context;
+using RestAPI.FluentValidation.Services.Repository;
+using RestAPI.FluentValidation.Services.Services;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder ( args );
+
+// Add services to the container.
+
+builder.Services.AddControllers ();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer ();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions> , ConfigureSwaggerOptions> ();
+builder.Services.AddSwaggerGen ( options =>
+{
+    // Add a custom operation filter which sets default values
+    options.OperationFilter<SwaggerDefaultValues> ();
+} );
+builder.Services
+    .AddApiVersioning ( options =>
+    {
+        //indicating whether a default version is assumed when a client does
+        // does not provide an API version.
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
+        options.DefaultApiVersion = new ApiVersion ( 1.0 );
+    } )
+    .AddApiExplorer ( options =>
+    {
+        // Add the versioned API explorer, which also adds IApiVersionDescriptionProvider service
+        // note: the specified format code will format the version as "'v'major[.minor][-status]"
+        options.GroupNameFormat = "'v'VVV";
+
+        // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+        // can also be used to control the format of the API version in route templates
+        options.SubstituteApiVersionInUrl = true;
+    } );
+
+// Add Repositories
+builder.Services.AddScoped<IItemRepository , ItemRepository> ();
+
+// Add Services
+builder.Services.AddScoped<IItemService , ItemService> ();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper ( typeof ( InventoryProfile ) );
+
+// Add Db Context
+builder.Services.AddDatabase<InventoryDBContext> ();
+
+var app = builder.Build ();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment ())
+{
+    app.UseSwagger ();
+    app.UseSwaggerUI ( options =>
+    {
+        var descriptions = app.DescribeApiVersions ();
+
+        // Build a swagger endpoint for each discovered API version
+        foreach (var description in descriptions)
+        {
+            options.SwaggerEndpoint ( $"/swagger/{description.GroupName}/swagger.json" , description.GroupName.ToUpperInvariant () );
+        }
+    } );
+}
+
+app.UseHttpsRedirection ();
+
+app.UseAuthorization ();
+
+app.MapControllers ();
+
+app.UseRouting ();
+
+app.Run ();
